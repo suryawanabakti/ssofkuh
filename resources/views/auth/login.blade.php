95cd6942d2ee8c0adfca743a236164a7d98c800b
surya <suryawanabakti18@gmail.com>
2024-01-20 13:18:15 +0800

    toogle

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,13 +21,13 @@
                     <div class="mb-2">
                         <label class="form-label">
                             Password
-
                         </label>
                         <div class="input-group input-group-flat">
                             <input type="password" class="form-control" placeholder="Your password" name="password"
-                                autocomplete="off" />
+                                autocomplete="off" id="password" />
                             <span class="input-group-text">
-                                <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip">
+                                <a href="#" class="link-secondary" data-bs-toggle="tooltip"
+                                    onclick="togglePassword()" id="toggle_password">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
                                         viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                                         stroke-linecap="round" stroke-linejoin="round">
@@ -64,4 +64,31 @@
             </div>
         </div>
     </div>
+    <script>
+        var boolean = true;
+
+        function togglePassword() {
+            var toggle_password = document.getElementById("toggle_password")
+            var password = document.getElementById("password")
+            boolean = !boolean
+
+            if (boolean) {
+
+                password.setAttribute("type", "password")
+                toggle_password.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                        <path
+                                            d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                                    </svg>`
+            } else {
+
+                password.setAttribute("type", "text")
+                toggle_password.innerHTML =
+                    `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" /><path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" /><path d="M3 3l18 18" /></svg>`
+            }
+        }
+    </script>
 </x-guest-layout>
