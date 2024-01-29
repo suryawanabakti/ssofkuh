984294c11845b552a6ad3d32ff483f504b4fffdc
surya <suryawanabakti18@gmail.com>
2024-01-29 22:36:48 +0800

    update

--- app/Http/Middleware/SSOMiddleware.php
+++ app/Http/Middleware/SSOMiddleware.php
@@ -15,7 +15,7 @@ class SSOMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->header('username') === env('USERNAME_SSO') && $request->header('password') === env("SECRET_KEY_SSO ")) {
+        if ($request->header('username') === env('USERNAME_SSO') && $request->header('password') === env("SECRET_KEY_SSO")) {
             return $next($request);
         } else {
             return response()->json([
