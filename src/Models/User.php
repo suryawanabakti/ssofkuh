7920ee8f5a7993e39111f406b86dc0d7eebd392b
surya <suryawanabakti18@gmail.com>
2024-02-21 13:35:27 +0800

    tst

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,102 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Support\Str;
-use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-
-    public $with = ['roles'];
-
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        // auto-sets values on creation
-        static::creating(function ($query) {
-            $query->uuid =  (string) Str::uuid();
-        });
-    }
-
-    public function getRouteKeyName()
-    {
-        return 'uuid';
-    }
-
-    /**
-     * Get the value indicating whether the IDs are incrementing.
-     *
-     * @return bool
-     */
-    public function getIncrementing()
-    {
-        return true;
-    }
-
-    /**
-     * Get the auto-incrementing key type.
-     *
-     * @return string
-     */
-    public function getKeyType()
-    {
-        return 'string';
-    }
-
-    protected $fillable = [
-        'name',
-        'username',
-        'password',
-        'nik',
-        'ttd',
-        'nomor',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    public function biodata()
-    {
-        return $this->hasOne(ResidenBiodata::class);
-    }
-
-    public function prodi()
-    {
-        return $this->hasOne(StudyProgram::class);
-    }
-
-    public function departemen()
-    {
-        return $this->hasOne(Department::class);
-    }
+    protected $guarded = ['id'];
 }
