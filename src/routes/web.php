0356bf21463d5b2d6f0d69924f7d6a14aaa0c8b3
surya <suryawanabakti18@gmail.com>
2024-02-21 17:15:31 +0800

    test

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
+
 Route::get("/active-user", function () {
-    return auth()->user();
+    return Http::withHeaders([
+        'username' => env('SSO_USERNAME'),
+        'password' => env('SSO_SECRET_KEY')
+    ])->get(env("SSO_URL" . "/get-sso-token"));
 });
