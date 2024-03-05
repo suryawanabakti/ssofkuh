b6fefea7fbaf9edb2a3015deeb1eac1a96123a04
SURYA  WANA BAKTI <115052947+suryawanabakti@users.noreply.github.com>
2024-03-05 15:21:16 +0800

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,30 +4,45 @@ use Surya\Sso\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Facades\Route;
 
-Route::get("/active-user/{user_id}", function ($user_id) {
-    $user = User::find($user_id);
-    $token = $user->createToken('ssofkuh')->plainTextToken;
-    $app_url = url('/');
-    return redirect(env('SSO_URL') . "/trusted-host?token=$token&app_url=$app_url");
-});
+Route::group([
+    'middleware' => 'web',
+], function ($router) {
+    require base_path('routes/web.php');
 
-Route::get('/get-token', function (Request $request) {
-    $token = PersonalAccessToken::findToken($request->token);
-    if (empty($token)) {
-        return "token tidak ditemukan";
-    }
-    return response()->json($token->tokenable()->first());
-});
+    $router->get("/active-user", function () {
+        $user =  auth()->user();
+        $token = $user->createToken('ssofkuh')->plainTextToken;
+        $app_url = url('/');
+        return redirect(env('SSO_URL') . "/trusted-host?token=$token&app_url=$app_url");
+    })->middleware('auth');
 
-Route::get('/update-sso-token', function (Request $request) {
 
-    $updated =  User::where('id', $request->user_id)->update([
-        'sso_token' => $request->sso_token
-    ]);
+    $router->get('/get-token', function (Request $request) {
+        $token = PersonalAccessToken::findToken($request->token);
+        if (empty($token)) {
+            return "token tidak ditemukan";
+        }
+        return response()->json($token->tokenable()->first());
+    });
 
-    return response()->json([
-        'message' => 'Berhasil terupdated',
-        'update' => $updated,
-    ]);
+    $router->get('/update-sso-token', function (Request $request) {
+
+        $updated =  User::where('id', $request->user_id)->update([
+            'sso_token' => $request->sso_token
+        ]);
+
+        if ($updated) {
+            return response()->json([
+                'message' => 'Berhasil terupdated',
+                'update' => $updated,
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Gagal update',
+                'update' => $updated,
+            ], 422);
+        }
+    });
 });
